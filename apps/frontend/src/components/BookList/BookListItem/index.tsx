2e6f01808547c9a697c4997c0f763e4663552452
David Amunga <david.onyimbo@gmail.com>
2024-06-12 22:58:07 +0300

    style: added framer motion

--- apps/frontend/src/components/BookList/BookListItem/index.tsx
+++ apps/frontend/src/components/BookList/BookListItem/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid, Button, useTheme, Typography, Stack } from "@mui/material";
 import { BookmarkAddRounded, RemoveCircle } from "@mui/icons-material";
 import { Book } from "@/@types/Book";
+import { motion } from "framer-motion";
 
 interface Props {
   book: Book;
@@ -24,38 +25,59 @@ export const BookListItem: React.FC<Props> = ({
       md={3}
       style={{ display: "flex", flexDirection: "column", gap: 2 }}
     >
-      <img
-        style={{ borderRadius: "4px" }}
-        src={book.coverPhotoURL}
-        width="100%"
-        alt={book.title}
-      />
-
-      <Typography gutterBottom variant="body1">
-        {book.title}
-      </Typography>
-      <Typography color={theme.palette.grey[500]} variant="body2">
-        By {book.author}
-      </Typography>
-      <div style={{ flex: 1 }}></div>
-      <Button
-        variant={"outlined"}
-        size="small"
-        sx={{ borderRadius: "50px" }}
-        color={isInReadingList ? `error` : `primary`}
-        startIcon={isInReadingList ? <RemoveCircle /> : <BookmarkAddRounded />}
-        onClick={() => {
-          //Since there is no unique id - concat with book title and reading level
-          let bookTitleReadingLevel = `${book.title}-${book.readingLevel}`;
-          if (isInReadingList) {
-            onRemoveFromReadingList(bookTitleReadingLevel);
-          } else {
-            onAddToReadingList(bookTitleReadingLevel);
-          }
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          height: "100%",
         }}
       >
-        {isInReadingList ? `Remove` : "Add"}
-      </Button>
+        <img
+          style={{ borderRadius: "4px" }}
+          src={book.coverPhotoURL}
+          width="100%"
+          alt={book.title}
+        />
+
+        <Typography gutterBottom variant="body1">
+          {book.title}
+        </Typography>
+        <Typography color={theme.palette.grey[500]} variant="body2">
+          By {book.author}
+        </Typography>
+        <div style={{ flex: 1 }}></div>
+        <motion.div
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.9 }}
+          style={{ width: "100%" }}
+        >
+          <Button
+            variant={"outlined"}
+            size="small"
+            fullWidth
+            sx={{ borderRadius: "50px" }}
+            color={isInReadingList ? `error` : `primary`}
+            startIcon={
+              isInReadingList ? <RemoveCircle /> : <BookmarkAddRounded />
+            }
+            onClick={() => {
+              //Since there is no unique id - concat with book title and reading level
+              let bookTitleReadingLevel = `${book.title}-${book.readingLevel}`;
+              if (isInReadingList) {
+                onRemoveFromReadingList(bookTitleReadingLevel);
+              } else {
+                onAddToReadingList(bookTitleReadingLevel);
+              }
+            }}
+          >
+            {isInReadingList ? `Remove` : "Add"}
+          </Button>
+        </motion.div>
+      </motion.div>
     </Grid>
   );
 };
