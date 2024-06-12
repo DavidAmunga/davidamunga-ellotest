294a2e783006f13c932a3ad5922992821f17b52b
David Amunga <david.onyimbo@gmail.com>
2024-06-12 11:43:47 +0300

    feat: setup gql client

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import type { AppProps } from "next/app";
+import { ApolloProvider } from '@apollo/client';
+import client from "@/graphql/client";
+
 const theme = createTheme({
   typography: {
     fontFamily: [
@@ -33,8 +36,10 @@ const theme = createTheme({
 });
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
